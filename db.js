2970a76e556299dd6256cd0d14df31f0142ca5e3
ashinamu <ashinamuhammed76@gmail.com>
2025-06-22 16:00:48 -0700

    Add env variable logging for debuggi

--- db.js
+++ db.js
@@ -1,6 +1,15 @@
 const mysql = require('mysql2');
 require('dotenv').config();
 
+console.log('DB_HOST:', process.env.DB_HOST);
+console.log('DB_USER:', process.env.DB_USER);
+console.log('DB_PASSWORD:', process.env.DB_PASSWORD ? '******' : 'Not set');
+console.log('DB_NAME:', process.env.DB_NAME);
+console.log('DB_PORT:', process.env.DB_PORT || 'Not set');
+
+// Then your MySQL connection code follows
+const mysql = require('mysql2');
+
 const db = mysql.createConnection({
   host: process.env.DB_HOST,
   user: process.env.DB_USER,
