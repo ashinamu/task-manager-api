cea8eba6d6e7a043aafb31e3d1f5646f05d2d3b9
ashinamu <ashinamuhammed76@gmail.com>
2025-06-22 15:46:22 -0700

    kj

--- db.js
+++ db.js
@@ -6,7 +6,7 @@ const db = mysql.createConnection({
   user: process.env.DB_USER,
   password: process.env.DB_PASSWORD,
   database: process.env.DB_NAME,
-   port: 3306  // Add this line
+   port: process.env.DB_PORT || 3306
 });
 
 db.connect(err => {
